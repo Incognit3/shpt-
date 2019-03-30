897f45eb01b56a81f77be8d10a78414506180118
McNuggets7778 <mockvich88@gmail.com>
2019-03-30 22:53:16 +0300

    30/03 v2

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
-
 $(document).ready(function(){
       $('.slider').bxSlider();
       adaptiveHeight: false;
       touchEnabled: true;
       pager: false;
-      responsive: false;
     });
 
-// var menuElem = document.getElementById('menu');
-
-// menuElem.onclick = function() {
-//       menuElem.classList.toggle('open');
-// };
-
 var form = document.getElementById('priem');
+var formasv = document.getElementById('form');
 
 form.onclick = function() {
-      form.classList.toggle('open');
+     formasv.classList.toggle('open');
+};
+
+
+var menu1 = document.getElementById('menu1');
+var menu2 = document.getElementById('menu2');
+var menulist = document.getElementById('menulist');
+var menu3 = document.getElementById('menu3');
+var menu4 = document.getElementById('menu4');
+
+menu1.onclick = function() {
+      menu1.classList.toggle('open');
+};
+menu2.onclick = function() {
+      menulist.classList.toggle('open');
+};
+menu3.onclick = function() {
+      menu3.classList.toggle('open');
+};
+menu4.onclick = function() {
+      menu4.classList.toggle('open');
 };
