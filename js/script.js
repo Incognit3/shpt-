06a2dcaac946825792d6d13b160c6882a31af742
McNuggets7778 <mockvich88@gmail.com>
2019-03-31 19:13:12 +0300

    deadline

--- js/script.js
+++ js/script.js
@@ -3,8 +3,24 @@ $(document).ready(function(){
       adaptiveHeight: false;
       touchEnabled: true;
       pager: false;
-    });
+      auto: true; pause: 1200;
+      
+      // mode: 'fade',  // тип перехода между слайдами может быть 'horizontal', 'vertical', 'fade'
+      // // captions: true,         // отображение title
+      // // easing: 'easeInOutQuad', // анимация слайда
+      // // controls: true,         // отображение стрелки - вперед, назад
+      // // startSlide: 0,          // Показ начнется с заданного слайда
+      // // infiniteLoop: true,     // показывать первый слайд за последним 
+      // // pager: true,            // показ номера страницы
+      // // auto: true,             // сделать автоматический переход
+      // // autoControls: true,     // показывает кнопку плей и стоп
+      // // video: true,            // включить видео
+      // // pause: 4000,            // время между сменой слайдов в м-сек
+      // // speed: 500,             // длительность перехода слайда в м-сек
+      // // useCSS: false           // CSS переходы
 
+});
+   
 var form = document.getElementById('priem');
 var formasv = document.getElementById('form');
 
